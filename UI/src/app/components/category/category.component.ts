3da2e005f5434002f6cb3bd202843b08b35f2bba
William Bjerke <bjerkewilliam@yahoo.com>
2022-05-19 22:41:57 -0500

    added setcategorydata

--- UI/src/app/components/category/category.component.ts
+++ UI/src/app/components/category/category.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CategoryService } from 'src/app/services/category/category.service';
+import { CategoryService } from '../../services/category/category.service';
 
 @Component({
   selector: 'app-category',
