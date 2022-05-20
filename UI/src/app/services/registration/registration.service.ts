e2180d749e120eaf3f8d4f91409e83384d46571e
William Bjerke <bjerkewilliam@yahoo.com>
2022-05-20 17:05:58 -0500

    updated tests

--- UI/src/app/services/registration/registration.service.ts
+++ UI/src/app/services/registration/registration.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { User } from '../../common/Models';
-
+import { path } from '../../common/defaultPath';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +12,6 @@ export class RegistrationService {
   constructor(private http: HttpClient) {}
    
   register(body: User): Observable<any>{
-    return this.http.post('http://localhost:8080/user', body);
+    return this.http.post(path +'/user', body);
   }
 }
--- src/test/java/com/revature/GroupDP2/TestUserService.java
+++ src/test/java/com/revature/GroupDP2/TestUserService.java
@@ -7,27 +7,31 @@ import com.revature.GroupDP2.model.User;
 import com.revature.GroupDP2.repository.UserRepository;
 import com.revature.GroupDP2.services.UserService;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class TestUserService {
-    private static UserRepository userRepository;
-    private static UserService userService;
-    private static User tu1;
-    private static User tu2;
+    @MockBean(UserRepository.class)
+    @Autowired
+    private UserRepository userRepository;
+    private UserService userService;
+    private User tu1;
+    private User tu2;
 
-    @BeforeAll
-    public static void beforeC(){
-        userRepository=mock(UserRepository.class);
-        userRepository=mock(UserRepository.class);
-        userService = new UserService(userRepository);
-    }
     @BeforeEach
     public void before(){
+        userService=new UserService(userRepository);
         tu1=new User("username","password",true,"John","Test",
                 "EMAIL@EMAIL.COM","phone","street","city","state","zip");
         tu2=new User("wrong","wrong",true,"Mark","Test",
