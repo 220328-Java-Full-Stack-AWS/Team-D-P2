00fb4829d514ba306c94ef9aa982349bd4f5d325
kettaleb <k.ettaleb@gmail.com>
2022-05-23 16:11:12 -0500

    Add payment class, component, and service the new UI

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductService } from './services/product.service';
 
 import { ProductComponent } from './components/product/product.component';
 import { NgModule } from '@angular/core';
+import { PaymentComponent } from './components/payment/payment.component';
 
 
 
@@ -35,6 +36,7 @@ const routes: Routes = [
   declarations: [
     AppComponent,
   ProductComponent,
+  PaymentComponent,
   
     
     
