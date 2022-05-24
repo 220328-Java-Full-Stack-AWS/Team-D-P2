0582e168c2c9f33db7b813196671ac9492cb8899
Laflammex <92762772+GioAmira@users.noreply.github.com>
2022-05-23 23:31:31 -0400

    working spring security

--- src/main/java/com/revature/GroupDP2/security/CorsFilter.java
+++ src/main/java/com/revature/GroupDP2/security/CorsFilter.java
@@ -10,13 +10,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Enumeration;
 
 
 @Component
